8cf7d4f3156440334143292a9b82ca83648d78f2
wessel-mulder <wesselmulder00@gmail.com>
2025-06-19 17:53:00 +0200

    edit plots

--- Scripts/4_additional_plots/distancesHistogram.R
+++ Scripts/4_additional_plots/distancesHistogram.R
@@ -1,9 +1,32 @@
 input <- 'Results/1_mahalanobis_outliers'
+output <- 'Plots/4_additional_plots/'
 
-df_allgenes <- read.csv(file.path(input,'distances_allgenes/distances_dnds.csv'))
-df_outliers <- read.csv(file.path(input,'outliers_genes/outliers_dnds_q95.csv'))
-breaks <- seq(0,1,by=0.0005)
+df_allgenes <- read.csv(file.path(input,'distances_allgenes/dnds.csv'))
+df_outliers <- read.csv(file.path(input,'outliers_genes/dnds_q95.csv'))
+breaks <- seq(0,1,by=0.005)
 hist(df_allgenes$pval,
      breaks=breaks,
      xlim=c(0,0.05))
+abline(v=0.05,col='red')
 
+threshold <- quantile(df_allgenes$distance,0.95)
+
+pdf(file.path(output,'dnds_distances.pdf'),
+    width = 8,
+    height = 4)
+hist(df_allgenes$distance,
+     main = 'Mahalanobis distances',
+     xlab = 'Distances')
+abline(v=threshold,col='red')
+dev.off()
+
+pdf(file.path(output,'dnds_outlier_pvalues.pdf'),
+    width = 8,
+    height = 4)
+breaks <- seq(0,1,by=0.005)
+hist(df_allgenes$pval,
+     breaks=breaks,
+     main = 'P-values of distances',
+     xlab = 'p-value',
+     xlim=c(0,1))
+dev.off()
